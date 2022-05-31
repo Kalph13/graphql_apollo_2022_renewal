dfd65b891a51dbc1fd52eb856d287fe39a4a2f3c
Younglong Kim <withel1013@gmail.com>
2022-06-01 05:53:59 +0900

    GraphQL Apollo Client

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Movies from "./routes/Movies";
+import Movie from "./routes/Movie";
+
 
 const App = () => {
   return (
-    <div></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Movies />} />
+        <Route path="/movies/:id" element={<Movie />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
