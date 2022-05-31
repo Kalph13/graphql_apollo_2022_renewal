a8ba649ba451f71996461cafd1111dcb5cc5233d
Younglong Kim <withel1013@gmail.com>
2022-05-31 20:45:47 +0900

    Initialization

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+/* Project Setting */
+/* Install Apollo Client & GraphQL: npm install @apollo/client graphql */
+/* Install react-router-dom v6: npm install react-router-dom@6 */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +12,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
